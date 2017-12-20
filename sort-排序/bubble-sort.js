f8969a309bdde5bd9f6deb50de538afff06afac6
wangwei <892458829@qq.com>
2017-12-20 17:01:13 +0800

    add

---
+++ sort-排序/bubble-sort.js
@@ -0,0 +1,17 @@
+var arr = [3,11,6,5,4,4,1]
+function sort(arr){
+	var j = arr.length
+	for(var k = 0;k<arr.length;k++){
+		for (var i = 0;i<j-1;i++){
+			var a = arr[i];
+			var b = arr[i+1]
+			if(arr[i]>arr[i+1]){
+				arr[i] = b;
+				arr[i+1] = a;
+			}			
+		}
+		j = j-1;
+	}
+	return arr;
+}
+console.log(sort(arr))
